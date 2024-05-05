ae912b416424385180d4350818e9f5a9d3c1972e
EstebanMa12 <desteban.maya@udea.edu.co>
2024-05-05 18:34:58 -0500

    chore: Update backend dependencies and add mongoose

--- Backend/src/config/server.ts
+++ Backend/src/config/server.ts
@@ -5,10 +5,14 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
+//MONGO
+import {connect} from "../config/database/mongoConfig";
+
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+connect()
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
